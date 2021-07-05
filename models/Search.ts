61b49009a6f5fd88768551dce222696d4053aef2
johng <jcguest90@gmail.com>
2021-07-05 15:04:18 -0500

    add getName()  and name/year properties to Search

--- models/Search.ts
+++ models/Search.ts
@@ -4,19 +4,20 @@ class Search {
   public query: string;
   public url: string;
   public raceName: string;
+  public year: string;
 
   constructor(searchQuery: string) {
     this.url = 'http://ergast.com/api/f1';
     this.query = searchQuery;
+    this.raceName = this.getName(searchQuery);
+    this.year = this.getYear(searchQuery);
   }
 
   async search() {
     let roundNumber = await this.findRound();
     let result = await axios
       .get(
-        `http://ergast.com/api/f1/${this.getYear(
-          this.query
-        )}/${roundNumber}/results/1.json`
+        `http://ergast.com/api/f1/${this.year}/${roundNumber}/results/1.json`
       )
       .then((json) => {
         const data = json.data;
@@ -38,19 +39,17 @@ class Search {
 
   async findRound() {
     let answer = await axios
-      .get(`http://ergast.com/api/f1/${this.getYear(this.query)}.json`)
+      .get(`http://ergast.com/api/f1/${this.year}.json`)
       .then((json) => {
         let data = json.data;
         return data.MRData.RaceTable.Races.find((race) => {
-          return (
-            race.raceName.toString().toLowerCase() == this.raceName
-          );
+          return race.raceName.toString().toLowerCase() == this.raceName;
         });
       })
       .catch((err) => console.error(err));
 
     return answer.round;
-  };
+  }
 
   getYear(query: string) {
     let array = query.split(' ');
@@ -65,6 +64,79 @@ class Search {
     });
     return year;
   }
+
+  getName(query: string) {
+    let gpNames = [
+      'australian',
+      'australia',
+      'bahrain',
+      'bahraini',
+      'china',
+      'chinese',
+      'azerbaijan',
+      'azeri',
+      'spain',
+      'spanish',
+      'monaco',
+      'monte carlo',
+      'canada',
+      'canadian',
+      'france',
+      'french',
+      'austria',
+      'austrian',
+      'great britain',
+      'british',
+      'germany',
+      'german',
+      'hungary',
+      'hungarian',
+      'belgium',
+      'belgian',
+      'italy',
+      'italian',
+      'singapore',
+      'russia',
+      'russian',
+      'japan',
+      'japanese',
+      'mexico',
+      'mexican',
+      'united states',
+      'america',
+      'american',
+      'brazil',
+      'brazilian',
+      'abu dhabi',
+      'malaysian',
+      'malaysia',
+      'dutch',
+      'european',
+      'portugal',
+      'portuguese',
+      'saudi arabia',
+      'saudi',
+      'saudi arabian'
+    ];
+    const queryArray = query.split(' ');
+    let name = '';
+
+    while (!name) {
+      for (let x = 0; x < gpNames.length; x++) {
+        for (let i = 0; i < queryArray.length; i++) {
+          gpNames[x] == queryArray[i].toLowerCase() ? (name = gpNames[x]) : -1;
+        }
+      }
+    }
+    let fullName = name.toLowerCase() + ' grand prix';
+    return fullName;
+  }
 }
+
+const Q = new Search('who won the 2009 Italian Grand Prix?');
+console.log(Q.query);
+console.log(Q.url);
+console.log(Q.raceName);
+console.log(Q.year);
 
 module.exports = Search;
--- string.js
+++ string.js
@@ -1,17 +1,278 @@
-let string = 'who won the 2005 Japanese grand prix';
+let string = 'who won the 2005 italian grand prix';
 
-function getYear(s) {
-  const array = s.split(' ');
-  const year = array.find((word) => {
-    const letters = word.split('');
-    if (letters[0] == '1') {
-      return word;
+// // function getYear(s) {
+// //   const array = s.split(' ');
+// //   const year = array.find((word) => {
+// //     const letters = word.split('');
+// //     if (letters[0] == '1') {
+// //       return word;
+// //     }
+// //     if (letters[0] == '2') {
+// //       return word;
+// //     }
+// //   });
+// //   console.log(year);
+// // }
+
+// // getYear(string);
+
+// function getName(s) {
+//   let allNames = [
+//     'australian',
+//     'australia',
+//     'bahrain',
+//     'bahraini',
+//     'china',
+//     'chinese',
+//     'azerbaijan',
+//     'azeri',
+//     'spain',
+//     'spanish',
+//     'monaco',
+//     'monte carlo',
+//     'canada',
+//     'canadian',
+//     'france',
+//     'french',
+//     'austria',
+//     'austrian',
+//     'great britain',
+//     'british',
+//     'germany',
+//     'german',
+//     'hungary',
+//     'hungarian',
+//     'belgium',
+//     'belgian',
+//     'italy',
+//     'italian',
+//     'singapore',
+//     'russia',
+//     'russian',
+//     'japan',
+//     'japanese',
+//     'mexico',
+//     'mexican',
+//     'united states',
+//     'america',
+//     'american',
+//     'brazil',
+//     'brazilian',
+//     'abu dhabi',
+//     'malaysian',
+//     'malaysia',
+//     'dutch',
+//     'european',
+//     'portugal',
+//     'portuguese',
+//     'saudi arabia',
+//     'saudi',
+//     'saudi arabian'
+//   ];
+//   const queryArray = s.split(' ');
+
+//   const name = queryArray.forEach((word) => {
+//     for (let i = 0; i < allNames.length - 1; i++) {
+//       // console.log(allNames[i]);
+//       if (allNames[i] == word.toLowercase) {
+//         name = word.toLowercase;
+//       }
+//     } // for loop
+//   }); // iterator
+//   return name;
+// } //function
+
+// function getName2(s) {
+//   let gpNames = [
+//     'australian',
+//     'australia',
+//     'bahrain',
+//     'bahraini',
+//     'china',
+//     'chinese',
+//     'azerbaijan',
+//     'azeri',
+//     'spain',
+//     'spanish',
+//     'monaco',
+//     'monte carlo',
+//     'canada',
+//     'canadian',
+//     'france',
+//     'french',
+//     'austria',
+//     'austrian',
+//     'great britain',
+//     'british',
+//     'germany',
+//     'german',
+//     'hungary',
+//     'hungarian',
+//     'belgium',
+//     'belgian',
+//     'italy',
+//     'italian',
+//     'singapore',
+//     'russia',
+//     'russian',
+//     'japan',
+//     'japanese',
+//     'mexico',
+//     'mexican',
+//     'united states',
+//     'america',
+//     'american',
+//     'brazil',
+//     'brazilian',
+//     'abu dhabi',
+//     'malaysian',
+//     'malaysia',
+//     'dutch',
+//     'european',
+//     'portugal',
+//     'portuguese',
+//     'saudi arabia',
+//     'saudi',
+//     'saudi arabian'
+//   ];
+//   const queryArray = s.split(' ');
+//   let name = '';
+
+//   for (let i = 0; i < queryArray.length; i++) {
+//     name = gpNames.find((gp) => {
+//       return gp == queryArray[i];
+//     });
+//   }
+// }
+
+// function getName3(s) {
+//   let gpNames = [
+//     'australian',
+//     'australia',
+//     'bahrain',
+//     'bahraini',
+//     'china',
+//     'chinese',
+//     'azerbaijan',
+//     'azeri',
+//     'spain',
+//     'spanish',
+//     'monaco',
+//     'monte carlo',
+//     'canada',
+//     'canadian',
+//     'france',
+//     'french',
+//     'austria',
+//     'austrian',
+//     'great britain',
+//     'british',
+//     'germany',
+//     'german',
+//     'hungary',
+//     'hungarian',
+//     'belgium',
+//     'belgian',
+//     'italy',
+//     'italian',
+//     'singapore',
+//     'russia',
+//     'russian',
+//     'japan',
+//     'japanese',
+//     'mexico',
+//     'mexican',
+//     'united states',
+//     'america',
+//     'american',
+//     'brazil',
+//     'brazilian',
+//     'abu dhabi',
+//     'malaysian',
+//     'malaysia',
+//     'dutch',
+//     'european',
+//     'portugal',
+//     'portuguese',
+//     'saudi arabia',
+//     'saudi',
+//     'saudi arabian'
+//   ];
+//   const queryArray = s.split(' ');
+//   let name = 'asdf';
+
+//   name = gpNames.find((name) => {
+//     for (let i = 0; i < queryArray.length; i++) {
+//       name == queryArray[i];
+//     }
+//   });
+//   return name;
+// }
+
+function getName4(s) {
+  let gpNames = [
+    'australian',
+    'australia',
+    'bahrain',
+    'bahraini',
+    'china',
+    'chinese',
+    'azerbaijan',
+    'azeri',
+    'spain',
+    'spanish',
+    'monaco',
+    'monte carlo',
+    'canada',
+    'canadian',
+    'france',
+    'french',
+    'austria',
+    'austrian',
+    'great britain',
+    'british',
+    'germany',
+    'german',
+    'hungary',
+    'hungarian',
+    'belgium',
+    'belgian',
+    'italy',
+    'italian',
+    'singapore',
+    'russia',
+    'russian',
+    'japan',
+    'japanese',
+    'mexico',
+    'mexican',
+    'united states',
+    'america',
+    'american',
+    'brazil',
+    'brazilian',
+    'abu dhabi',
+    'malaysian',
+    'malaysia',
+    'dutch',
+    'european',
+    'portugal',
+    'portuguese',
+    'saudi arabia',
+    'saudi',
+    'saudi arabian'
+  ];
+  const queryArray = s.split(' ');
+  let name = '';
+
+  while (!name) {
+    for (let x = 0; x < gpNames.length; x++) {
+      for (let i = 0; i < queryArray.length; i++) {
+        gpNames[x] == queryArray[i] ? (name = gpNames[x]) : -1;
+      }
     }
-    if (letters[0] == '2') {
-      return word;
-    }
-  });
-  console.log(year);
+  }
+  return name;
 }
 
-getYear(string);
+console.log(getName4(string));
